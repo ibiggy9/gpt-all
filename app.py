7bd3ae565cd26ef6b98d4f1afb0788dfd22cd178
Main <main@MacBook-Pro-2.local>
2024-06-15 11:22:38 -0400

    improved security

--- app.py
+++ app.py
@@ -6,27 +6,24 @@ from openai import OpenAI
 from typing import List
 import random
 from urllib.parse import urlparse
-from fastapi import FastAPI, Request, HTTPException, Depends
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 import yt_dlp
-from urllib.parse import urlparse
 import jwt
-from jwt import  DecodeError, decode
+from jwt import DecodeError, decode
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import re
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Load environment variables from .env file
 
-key = "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"
-client=OpenAI(api_key=key)
-SECRET_KEY = 'ASDGRTHHK@#K$%@#@!#%235283yhth7io1234hf783421g@$G$Y@45g7h24589gh5b2g@F!@#$F1f!Uf!YU(@UF(#H!H#K!FY(#(!&THFK!H@#$GH!THQEWKTQESTUGQERGHQJRGFB@J))))'
-ALGORITHM='HS256'
+key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=key)
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 security = HTTPBearer()
 device_id_pattern = re.compile(r'^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}[0-9A-Z]+$')
-
-
 
 app = FastAPI()
 app.add_middleware(
@@ -38,12 +35,11 @@ app.add_middleware(
 )
 
 class Message(BaseModel):
-    role:str
-    content:str
+    role: str
+    content: str
 
 class DeviceIDRequest(BaseModel):
     device_id: str
-
 
 async def get_current_device(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
@@ -56,23 +52,19 @@ async def get_current_device(credentials: HTTPAuthorizationCredentials = Depends
         return device_id
     except DecodeError:
         raise HTTPException(status_code=401, detail="Invalid JWT")
-    
 
 @app.get("/protected_endpoint")
 async def protected_endpoint(token: str = Depends(get_current_device)):
     return {"message": f"Access granted for device {token}"}
 
-
-
 @app.post("/gpt/knowledgeBase")
 async def knowledgeResponse(conversation: List[Message]):
     print(conversation)
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4o-2024-05-13",
         messages=conversation
     )
     return completion.choices[0].message
-
 
 @app.post("/heartbeat")
 async def get_token(request: DeviceIDRequest):
@@ -88,7 +80,6 @@ async def get_token(request: DeviceIDRequest):
 @app.get("/getAudioSize")
 async def get_audio_size(link: str, bitrate: int = 190):
     try:
-        # Extract video information
         parsed_url = urlparse(link)
         domain = parsed_url.netloc
 
@@ -97,16 +88,14 @@ async def get_audio_size(link: str, bitrate: int = 190):
             'noplaylist': True,
             'playlistend': 1,
         }
-        
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             duration = info_dict.get('duration', 0)  # Duration in seconds
             video_title = info_dict.get('title', 'Unknown')  # Video title
 
-        # Calculate file size in bytes
         file_size_bytes = (duration * bitrate * 1000) / 8  # bitrate is converted from kbps to bps
 
-        # Convert file size to megabytes for a more human-readable format
         file_size_mb = file_size_bytes / (1024 * 1024)
         file_size_mb = round(file_size_mb, 2)
 
@@ -115,7 +104,6 @@ async def get_audio_size(link: str, bitrate: int = 190):
     except Exception as e:
         return {"status": "error", "detail": str(e)}
 
-
 @app.post('/gpt/flourish')
 async def getFleurResponse(conversation: List[Message], token: str = Depends(get_current_device)):
     print(token)
@@ -123,15 +111,11 @@ async def getFleurResponse(conversation: List[Message], token: str = Depends(get
     
     print(conversation)
     completion = client.chat.completions.create(
-        model="gpt-4-1106-preview",
+        model="gpt-4o-2024-05-13",
         messages=conversation
     )
     return completion
 
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=4242)
-
-
-
